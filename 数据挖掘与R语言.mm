28466dbc46be68b9c7f3d499f43a377699606ddf
挖掘机大王子 <binyoucaixiaobin@qq.com>
2018-04-07 12:56:18 +0800

    思维导图源文件

---
+++ 数据挖掘与R语言.mm
@@ -0,0 +1,317 @@
+<map>
+		<node ID="root" TEXT="数据挖掘与R语言">		<node TEXT="第1章 简介 " ID="621629e27c62505c" STYLE="bubble" POSITION="right">
+		<node TEXT="1.1 如何阅读本书 " ID="1ee1629e27de02069" STYLE="fork">
+		</node>
+		<node TEXT="1.2 R简介 " ID="361629e27e54e11e" STYLE="fork">
+		</node>
+		<node TEXT="2.1 R起步 " ID="1ff1629e27ec7c12e" STYLE="fork">
+		</node>
+		<node TEXT="1.2.2 R对象" ID="2b21629e27f20e03e" STYLE="fork">
+		</node>
+		<node TEXT="1.2.3 向量" ID="1551629e27f72f12a" STYLE="fork">
+		</node>
+		<node TEXT="1.2.4 向量化" ID="18d1629e27fb750a7" STYLE="fork">
+		</node>
+		<node TEXT="1.2.5 因子 " ID="25f1629e2801ce084" STYLE="fork">
+		</node>
+		<node TEXT="1.2.6 生成序列 " ID="1df1629e28078c008" STYLE="fork">
+		</node>
+		<node TEXT="1.2.7 数据子集" ID="1f61629e280c3f036" STYLE="fork">
+		</node>
+		<node TEXT="1.2.8 矩阵和数组" ID="1341629e28121516e" STYLE="fork">
+		</node>
+		<node TEXT="1.2.9 列表 " ID="a41629e28183912a" STYLE="fork">
+		</node>
+		<node TEXT="1.2.10 数据框" ID="1851629e281e6a14" STYLE="fork">
+		</node>
+		<node TEXT="1.2.11 构建新函数 " ID="3301629e28241d0cf" STYLE="fork">
+		</node>
+		<node TEXT="1.2.12 对象、类和方法 " ID="22b1629e282d94091" STYLE="fork">
+		</node>
+		<node TEXT="1.2.13 管理R会话 " ID="9e1629e283e05131" STYLE="fork">
+		</node>
+		<node TEXT="1.3 MySQL 简介" ID="3e71629e286ae409e" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT=" 第2章 预测海藻数量 " ID="39b1629e290f9811e" STYLE="bubble" POSITION="right">
+		<node TEXT="2.1 问题描述与目标" ID="1e21629e2919a507d" STYLE="fork">
+		</node>
+		<node TEXT="2.2 数据说明 " ID="21a1629e291e6c082" STYLE="fork">
+		</node>
+		<node TEXT="2.3 数据加载到R " ID="1a71629e2925f4016" STYLE="fork">
+		</node>
+		<node TEXT="2.4 数据可视化和摘要" ID="1f01629e292bb9139" STYLE="fork">
+		</node>
+		<node TEXT="2.5 数据缺失 " ID="2cf1629e2931ac0ed" STYLE="fork">
+		<node TEXT="2.5.1 将缺失部分剔除 " ID="1451629e293713123" STYLE="fork">
+		</node>
+		<node TEXT="2.5.2 用最高频率值来填补缺失值 " ID="3031629e293f450fe" STYLE="fork">
+		</node>
+		<node TEXT="2.5.3 通过变量的相关关系来填补缺失值 " ID="d81629e2948ae123" STYLE="fork">
+		</node>
+		<node TEXT="2.5.4 通过探索案例之间的相似性来填补缺失值 " ID="1f51629e2950d216a" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="2.6 获取预测模型" ID="2341629e2961250da" STYLE="fork">
+		<node TEXT="2.6.1 多元线性回归 " ID="9b1629e296660003" STYLE="fork">
+		</node>
+		<node TEXT="2.6.2 回归树 " ID="1991629e296bd5175" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="2.7 模型的评价和选择" ID="2de1629e29724e15a" STYLE="fork">
+		</node>
+		<node TEXT="2.8 预测7类海藻的频率 " ID="36a1629e297b8a18c" STYLE="fork">
+		</node>
+		<node TEXT="2.9 小结" ID="10d1629e298a5f0e5" STYLE="fork">
+		<node TEXT="就数据挖掘而言，本章描述了这些内容" ID="19d1629e4bedf411a" STYLE="fork">
+		<node TEXT="数据可视化" ID="2ef1629e4cad2411c" STYLE="fork">
+		</node>
+		<node TEXT="描述性统计分析" ID="26b1629e4cc31406d" STYLE="fork">
+		</node>
+		<node TEXT="处理缺失值的策略" ID="2491629e4cd83b126" STYLE="fork">
+		</node>
+		<node TEXT="回归分析" ID="32a1629e4d01f1185" STYLE="fork">
+		</node>
+		<node TEXT="回归分析的评价指标" ID="1831629e4d15b00f4" STYLE="fork">
+		</node>
+		<node TEXT="多元线性回归" ID="3841629e4d352e18e" STYLE="fork">
+		</node>
+		<node TEXT="回归树" ID="39e1629e4d6413139" STYLE="fork">
+		</node>
+		<node TEXT="通过k折交互验证来进行模型选择和比较" ID="2c11629e4d7245095" STYLE="fork">
+		</node>
+		<node TEXT="模型组合和随机森林" ID="1cd1629e4e076f109" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="获得的R技能" ID="2791629e4e931b081" STYLE="fork">
+		<node TEXT="读入文本数据文件" ID="2ac1629e4eefa709f" STYLE="fork">
+		</node>
+		<node TEXT="如何得到数据的描述性统计量" ID="ba1629e4f1b1c0f" STYLE="fork">
+		</node>
+		<node TEXT="基本的数据可视化方法" ID="2201629e4f6746054" STYLE="fork">
+		</node>
+		<node TEXT="如何构建回归模型" ID="d41629e4f95a3043" STYLE="fork">
+		</node>
+		<node TEXT="如何应用模型得到测试集的预测值" ID="33e1629e4fb9570fb" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		</node>
+		<node TEXT="第3章 预测股票市场收益" ID="dd1629e2ae5fd11" STYLE="bubble" POSITION="right">
+		<node TEXT=" 3.1 问题描述与目标" ID="21a1629e2aecbe048" STYLE="fork">
+		<node TEXT=" 3.2 可用的数据 " ID="3e51629e2af2be01e" STYLE="fork">
+		</node>
+		<node TEXT="3.21 在R中处理与时间有关的数据 " ID="1a1629e2af9e001e" STYLE="fork">
+		</node>
+		<node TEXT="3.22 从CSV文件读取数据" ID="3111629e2b044202d" STYLE="fork">
+		</node>
+		<node TEXT="3.23 从网站上获取数据 " ID="3271629e2b0a05151" STYLE="fork">
+		</node>
+		<node TEXT="3.24 从MySQL数据库读取数据" ID="3ac1629e2b0f60116" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="3.3 定义预测任务 " ID="2d31629e2cb7e60c7" STYLE="fork">
+		<node TEXT="3.3.1 预测什么" ID="2a41629e2b1731081" STYLE="fork">
+		</node>
+		<node TEXT="3.3.2 预测变量是什么 " ID="2081629e2b21b8156" STYLE="fork">
+		</node>
+		<node TEXT="3.3.3 预测任务 " ID="3dc1629e2b28310ee" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="3.3.4 模型评价准则 " ID="e61629e2b2e70015" STYLE="fork">
+		</node>
+		<node TEXT="3.4 预测模型 " ID="35b1629e2b34b8149" STYLE="fork">
+		<node TEXT="3.4.1 如何应用训练集数据来建模 " ID="1be1629e2b42f209" STYLE="fork">
+		</node>
+		<node TEXT="3.4.2 建模工具 " ID="1da1629e3cc84f16" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="3.5从预测到实践" ID="a01629e2b4a0714a" STYLE="fork">
+		<node TEXT="3.5.1 如何应用预测模型 " ID="11629e2b555a174" STYLE="fork">
+		</node>
+		<node TEXT="3.5.2 与交易相关的评价准则 " ID="20b1629e2b599316e" STYLE="fork">
+		</node>
+		<node TEXT="3.5.3 模型集成：仿真交易 " ID="2461629e2b75fe02" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="3.6 模型评价和选择 " ID="11e1629e2b7e8f026" STYLE="fork">
+		<node TEXT="3.6.1 蒙特卡罗估计" ID="3e01629e2b838416b" STYLE="fork">
+		</node>
+		<node TEXT="3.6.2 实验比较 " ID="641629e2b895008e" STYLE="fork">
+		</node>
+		<node TEXT="3.6.3 结果分析 " ID="3d41629e2b8e9004b" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="3.7 交易系统 " ID="1d51629e2b9876113" STYLE="fork">
+		<node TEXT="3.7.1 评估最终测试数据" ID="e41629e2b9f2607a" STYLE="fork">
+		</node>
+		<node TEXT="3.7.2 在线交易系统" ID="441629e2ba4af02f" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="3.8 小结 " ID="ce1629e2baec7075" STYLE="fork">
+		<node TEXT="从方法术语的角度，本章介绍了这些新的主题" ID="28c1629e51c727146" STYLE="fork">
+		<node TEXT="时间序列建模" ID="25b1629e5346b60c" STYLE="fork">
+		</node>
+		<node TEXT="处理带有窗口机制的模式变化" ID="2c01629e53676f0df" STYLE="fork">
+		</node>
+		<node TEXT="人工神经网络" ID="1141629e53a9600b9" STYLE="fork">
+		</node>
+		<node TEXT="支持向量机" ID="19c1629e54543a01d" STYLE="fork">
+		</node>
+		<node TEXT="多远自适应回归样条" ID="2b51629e547484168" STYLE="fork">
+		</node>
+		<node TEXT="用蒙特卡洛方法来评估时间序列模型" ID="1231629e54c50e14b" STYLE="fork">
+		</node>
+		<node TEXT="几个预测稀有事件或者金融交易系统性能的新的标准评价指标" ID="711629e55154514d" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="从学习R的角度，我们演示了" ID="16e1629e559b1b061" STYLE="fork">
+		<node TEXT="如何处理时间序列" ID="37b1629e5624da0c6" STYLE="fork">
+		</node>
+		<node TEXT="如何从不同的来源读入数据，例如数据库" ID="161629e5650e4178" STYLE="fork">
+		</node>
+		<node TEXT="如何获得多种的模型（支持向量机、神经网络、多远自适应回归样条）" ID="2bf1629e56a4d9145" STYLE="fork">
+		</node>
+		<node TEXT="如何应用多个用于金融模型的R添加包" ID="2ba1629e5754d917f" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		</node>
+		<node TEXT="第4章 侦测欺诈交易 " ID="2031629e2ea297181" STYLE="bubble" POSITION="right">
+		<node TEXT="4.1 问题描述与目标 " ID="d01629e2ea92405b" STYLE="fork">
+		</node>
+		<node TEXT="4.2 可用的数据 " ID="371629e2eb43d137" STYLE="fork">
+		<node TEXT="4.2.1 加载数据至R " ID="7a1629e2ec38608b" STYLE="fork">
+		</node>
+		<node TEXT="4.2.2 探索数据集 " ID="36d1629e2ec9f6109" STYLE="fork">
+		</node>
+		<node TEXT="4.2.3 数据问题 " ID="201629e2ecf9f046" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="4.3 定义数据挖掘任务 " ID="1671629e2ee3ad02d" STYLE="fork">
+		<node TEXT="4.3.1 问题的不同解决方法 " ID="2071629e2eed4d0a7" STYLE="fork">
+		</node>
+		<node TEXT="4.3.2 评价准则 " ID="381629e2ef44f177" STYLE="fork">
+		</node>
+		<node TEXT="4.3.3 实验方法 " ID="161629e2efcac132" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="4.4 计算离群值的排序 " ID="2a61629e2f0295174" STYLE="fork">
+		<node TEXT="4.4.1 无监督方法 " ID="12f1629e2f094a061" STYLE="fork">
+		</node>
+		<node TEXT="4.4.2 有监督方法 " ID="2d41629e2f10ba0fe" STYLE="fork">
+		</node>
+		<node TEXT="4.4.3 半监督方法 " ID="1e01629e2f1940111" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="4.5 小结 " ID="38a1629e3001ec07a" STYLE="fork">
+		<node TEXT="用方法论的术语来讲，我们介绍来这些新的主题" ID="3cc1629e5a4232041" STYLE="fork">
+		<node TEXT="离群值监测和排序" ID="1101629e5ad29b0d2" STYLE="fork">
+		</node>
+		<node TEXT="聚类方法" ID="37f1629e5b45c3179" STYLE="fork">
+		</node>
+		<node TEXT="半监督学习" ID="3261629e5b6c4c0b4" STYLE="fork">
+		</node>
+		<node TEXT="通过自我训练的半监督分类" ID="21c1629e5b8d840c3" STYLE="fork">
+		</node>
+		<node TEXT="失衡的类分布以及处理这类问题的方法" ID="3d91629e5bd6810c" STYLE="fork">
+		</node>
+		<node TEXT="简单的贝叶斯分类器" ID="35e1629e5c224413d" STYLE="fork">
+		</node>
+		<node TEXT="AdaBoost 分类器" ID="bf1629e5c723013a" STYLE="fork">
+		</node>
+		<node TEXT="决策精准度/回溯精准度和累计回溯精准度" ID="421629e5ccc2501c" STYLE="fork">
+		</node>
+		<node TEXT="保留（交叉验证）实验" ID="1211629e5d8bfb0e5" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="从学习R的角度来讲，我们演示了" ID="1c01629e5db58e07b" STYLE="fork">
+		<node TEXT="如何获取多个性能指标统计量和如何应用ROCR添加包来可视化它们" ID="541629e5e1d870ee" STYLE="fork">
+		</node>
+		<node TEXT="如何得到性能指标统计量的交叉验证估计" ID="931629e5ee4bf058" STYLE="fork">
+		</node>
+		<node TEXT="如何用LOF方法得到局部离群值因子" ID="32f1629e5f5b2417a" STYLE="fork">
+		</node>
+		<node TEXT="如何用OR方法得到离群值排序" ID="1371629e5fd299083" STYLE="fork">
+		</node>
+		<node TEXT="如何用SMOTE方法克服类失衡" ID="29f1629e60d78a078" STYLE="fork">
+		</node>
+		<node TEXT="如何得到简单的贝叶斯分类模型" ID="91629e6124bd06e" STYLE="fork">
+		</node>
+		<node TEXT="如何得到AdaBoost. M1 分类模型" ID="da1629e61714c0e9" STYLE="fork">
+		</node>
+		<node TEXT="如何通过 RWeka 添加包来应用 Weka 数据挖掘系统的方法" ID="2d11629e61ee9c067" STYLE="fork">
+		</node>
+		<node TEXT="如何用自训方法把一个分类器应用到半监督的数据集" ID="691629e6280ca0cc" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		</node>
+		<node TEXT="第5章 微阵列样本分类 " ID="27e1629e2f26d20d8" STYLE="bubble" POSITION="right">
+		<node TEXT="5.1 问题描述与目标 " ID="2be1629e2f322f0d2" STYLE="fork">
+		<node TEXT="5.1.1 微阵列实验背景简介 " ID="15c1629e2f3a420c3" STYLE="fork">
+		</node>
+		<node TEXT="5.1.2 数据集ALL " ID="3901629e2f4913158" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="5.2 可用的数据 " ID="1461629e2f5b1d0b" STYLE="fork">
+		<node TEXT="5.3 基因（特征）选择 " ID="3351629e2f64bf158" STYLE="fork">
+		</node>
+		<node TEXT="5.3.1 基于分布特征的简单过滤方法 " ID="2e21629e2f7483021" STYLE="fork">
+		</node>
+		<node TEXT="5.3.2 ANOVA过滤 " ID="1701629e2f7b07199" STYLE="fork">
+		</node>
+		<node TEXT="5.3.3 用随机森林进行过滤 " ID="2731629e2f825909c" STYLE="fork">
+		</node>
+		<node TEXT="5.3.4 用特征聚类的组合进行过滤 " ID="1631629e2f899d194" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="5.4 遗传学异常的预测 " ID="2251629e2f8fd1094" STYLE="fork">
+		<node TEXT="5.4.1 定义预测任务 " ID="1b01629e2f9d6f14f" STYLE="fork">
+		</node>
+		<node TEXT="5.4.2 模型评价标准 " ID="881629e2fac9a13e" STYLE="fork">
+		</node>
+		<node TEXT="5.4.3 实验过程 " ID="391629e2fb7be023" STYLE="fork">
+		</node>
+		<node TEXT="5.4.4 建模技术 " ID="3b41629e2fbda20a" STYLE="fork">
+		</node>
+		<node TEXT="5.4.5 模型比较 " ID="1a01629e2fc51301a" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="5.5 小结" ID="37a1629e2670740b2" STYLE="fork">
+		<node TEXT="就数据挖掘概念而言，本章着重于这些主题" ID="25f1629e63f641057" STYLE="fork">
+		<node TEXT="有大量预测变量的问题的特征选择方法" ID="551629e64647f025" STYLE="fork">
+		</node>
+		<node TEXT="分类方法" ID="2ac1629e64a62a195" STYLE="fork">
+		</node>
+		<node TEXT="随机森林" ID="3371629e64b85b048" STYLE="fork">
+		</node>
+		<node TEXT="k 近邻方法" ID="25b1629e64cad70f3" STYLE="fork">
+		</node>
+		<node TEXT="支持向量机" ID="fd1629e654bf717e" STYLE="fork">
+		</node>
+		<node TEXT="用不同的预测变量集合进行组合" ID="3691629e655b1903" STYLE="fork">
+		</node>
+		<node TEXT="弃一交叉验证法" ID="2321629e658a39169" STYLE="fork">
+		</node>
+		</node>
+		<node TEXT="对R而言，我们学习来 几种新的技术" ID="cf1629e65ecd210a" STYLE="fork">
+		<node TEXT="如何处理微阵列数据" ID="3b61629e66470c122" STYLE="fork">
+		</node>
+		<node TEXT="应用ANOVA 检验比较几组数据的均值" ID="19a1629e66a70e0fc" STYLE="fork">
+		</node>
+		<node TEXT="在分类问题中用随机森林选择变量" ID="f31629e66fda511e" STYLE="fork">
+		</node>
+		<node TEXT="变量聚类" ID="8b1629e675c2316f" STYLE="fork">
+		</node>
+		<node TEXT="获得用不同预测 变量简历的模型组合" ID="1291629e6787bd08c" STYLE="fork">
+		</node>
+		<node TEXT="获得 k 近邻模型" ID="3551629e67c977188" STYLE="fork">
+		</node>
+		<node TEXT="用弃一交叉验证法估计模型的精确度" ID="1391629e67f50c0b3" STYLE="fork">
+		</node>
+		</node>
+		</node>
+		</node>
+</node>
+</map>
